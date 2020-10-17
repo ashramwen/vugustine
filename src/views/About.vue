b720c795309c995e5e903c79ccf098ab64c65e21
Augustine <ashramwen@gmail.com>
2020-10-17 17:28:18 +0800

    feat: 🎸 Complete base

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,4 +2,19 @@
   <div class="about">
     <h1>This is an about page</h1>
   </div>
+  <button @click="count++">count is: {{ count }}</button>
 </template>
+
+<script lang="ts">
+export default {
+  name: 'About',
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      count: 0,
+    };
+  },
+};
+</script>
